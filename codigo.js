ed5f3f6e3a1fdbc7f8780cad013b824d79d1e719
elienrique <benitezelian74@gmail.com>
2021-03-03 08:20:24 -0500

    inicio proyecto

---
+++ codigo.js
@@ -0,0 +1,5 @@
+function multiplicar(){
+    var n1 = parseInt(document.getElementById("a").value);
+    var n2 = parseInt(document.getElementById("b").value);
+ document.getElementById("resultado").innerHTML = n1 * n2;
+}
